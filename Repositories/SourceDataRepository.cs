7ec0e7c507e2f1fc7452af4c90c4302de2827df5
sh4npeiris <sh4npeiris@gmail.com>
2025-08-15 17:51:17 -0400

    Update SourceDataRepository.cs
    remove comments

--- Repositories/SourceDataRepository.cs
+++ Repositories/SourceDataRepository.cs
@@ -25,28 +25,6 @@ namespace ReportingDataSync.Repositories
 
         public async Task<DateTime?> GetMaxIncrementalValueAsync(string query, string incrementalColumn, DateTime lastRunDate)
         {
-            //// Preserve original formatting
-            //string sanitizedQuery = query
-            //    .Replace("\r", " ")
-            //    .Replace("\n", " ")
-            //    .Replace("  ", " ")
-            //    .Trim();
-
-            //// Handle aliased columns (e.g., "T.DateCreated")
-            //string qualifiedColumn = incrementalColumn.Contains('.')
-            //    ? incrementalColumn
-            //    : $"[{incrementalColumn}]";
-
-            //var scalarSql = $@"
-            //    SELECT MAX({qualifiedColumn})
-            //    FROM ({sanitizedQuery}) AS T
-            //";
-
-            //using var cmd = new SqlCommand(scalarSql, _productionConnection);
-            //cmd.Parameters.AddWithValue("@lastRunDate", lastRunDate);
-            //var result = await cmd.ExecuteScalarAsync();
-
-            //return result as DateTime?;
             // This regex finds the "SELECT ... FROM" part of the query, ignoring case.
             var selectPattern = new Regex(@"SELECT\s+.*?\s+FROM", RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
